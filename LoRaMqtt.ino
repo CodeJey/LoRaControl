921bb2d107f1768893df89d0012a05633c9340db
Denny <40717229+CodeJey@users.noreply.github.com>
2021-10-06 10:11:36 +0300

    Update LoRaMqtt.ino

--- LoRaMqtt.ino
+++ LoRaMqtt.ino
@@ -3,19 +3,28 @@
 #include <SPI.h>
 #include <LoRa.h>
 
+//defining pins for sensors
 #define DHTPIN1 3
 #define DHTPIN2 4
 #define DHTTYPE1 DHT11 
 #define DHTTYPE2 DHT22
+//defining the semsors 
 DHT dht1(DHTPIN1, DHTTYPE1);
 DHT dht2(DHTPIN2, DHTTYPE2);
+//def the IR distance sensor pin
 #define IRPin A0
 #define model 20150
+//array for sensor data
 char tem_1[8]={"\0"},hum_1[8]={"\0"}, tem_2[8]={"\0"},hum_2[8]={"\0"}, dis[8]={"\0"};
+//def the node id(BOARD)
 const char *node_id = ("<4567>");  //From LG01 via web Local Channel settings on MQTT.Please refer <> dataformat in here. 
+//def package length
 uint8_t datasend[42];
+//counter start
 unsigned int count = 1; 
+//refresh time for receiving/sending
 unsigned long new_time,old_time=0;
+//def the IR distance sensor
 SharpIR mySensor = SharpIR(IRPin, model);
 
 void setup()
@@ -49,7 +58,7 @@ void setup()
 
 void dhtTem()
 {
-       
+       //dht data method
        float tem = dht1.readHumidity();      
        float hum = dht1.readTemperature();             
        Serial.println(F("The temperature and humidity:"));
@@ -64,10 +73,12 @@ void dhtTem()
 }
 int distF()
 {
-  return mySensor.distance();
+      //distance sensor f
+      return mySensor.distance();
 }
 void dhtWrite()
 {
+      //reading data
     float tem,hum,tem1,hum1;
     tem = dht1.readTemperature();
     delay(100);
@@ -77,11 +88,12 @@ void dhtWrite()
     delay(100);
     hum1 = dht2.readHumidity();
     char data[50] = "\0";
+      //setting data
     for(int i = 0; i < 50; i++)
     {
        data[i] = node_id[i];
     }
-
+      //setting data seq in the data array
     dtostrf(tem,0,1,tem_1);
     dtostrf(hum,0,1,hum_1);
     dtostrf(tem1,0,1,tem_2);
@@ -89,6 +101,7 @@ void dhtWrite()
     dtostrf(distF(),0,1,dis);
 
     // Serial.println(tem_1);
+      //getting the data in data array
      strcat(data,"t1=");
      strcat(data,tem_1);
      strcat(data,"&h1=");
